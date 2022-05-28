e477ef4a87d45d5c6760c37f0ddf10b146248207
Tomas Scerbak <tomas.scerbak@gmail.com>
2022-05-28 11:24:28 +0200

    changing comment position

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const bill = document.querySelector('#bill');
 
 
+
 const fivePercentBtn = document.querySelector('#five');
 const tenPercentBtn = document.querySelector('#ten');
 const fifteenPercentBtn = document.querySelector('#fifteen');
@@ -13,27 +14,33 @@ const customBtn = document.querySelector('#custom');
 const peopleCountInput = document.querySelector('#people-count');
 const error = document.querySelector('.error');
 
-const tipPerPerson = document.querySelector('.tip-value');
-const totalPerPerson = document.querySelector('.total-value');
+let tipPerPerson = document.querySelector('.tip-value');
+let totalPerPerson = document.querySelector('.total-value');
 
 const resetBtn = document.querySelector('.btn--reset');
 
-
-
 // Getting value from bill input
+let inputValue = 0;
+
+
 bill.addEventListener('input', (e) => {
 
     // Getting input value and converting to number
     value = Number(e.target.value);
-    return value;
+    inputValue = value
+    console.log(value);
 
 })
+
+let total = 0;
+let people = 0;
 
 // Getting value from number of people input
 peopleCountInput.addEventListener('input', (e) => {
 
     //Converting input from string to number
     numberOfPeople = Number(e.target.value);
+    people = numberOfPeople;
 
     if (numberOfPeople === 0) {
         error.innerHTML = "Can't be zero";
@@ -45,34 +52,44 @@ peopleCountInput.addEventListener('input', (e) => {
         error.classList.remove('error');
         peopleCountInput.classList.remove('input-error');
         peopleCountInput.classList.add('input-onfocus');
+        let summary = (total / people).toFixed(2);
+        tipPerPerson.innerHTML = summary;
+        let parsedSummary = parseFloat(summary);
+        let parsedTotal = parseFloat(inputValue);
+        let parsedPeople = parseFloat(people);
+        totalPerPerson.innerHTML = (((parsedSummary * parsedPeople) + parsedTotal) / parsedPeople).toFixed(2);
     }
 })
 
 
-
 fivePercentBtn.addEventListener('click', () => {
-    let tip = ((value / 100) * 5).toFixed(2);
+    let tip = ((inputValue / 100) * 5).toFixed(2);
     tipPerPerson.innerHTML = tip;
+    total = tip;
 })
 
 tenPercentBtn.addEventListener('click', () => {
-    let tip = ((value / 100) * 10).toFixed(2);
+    let tip = ((inputValue / 100) * 10).toFixed(2);
     tipPerPerson.innerHTML = tip;
+    total = tip;
 })
 
 fifteenPercentBtn.addEventListener('click', () => {
-    let tip = ((value / 100) * 15).toFixed(2);
+    let tip = ((inputValue / 100) * 15).toFixed(2);
     tipPerPerson.innerHTML = tip;
+    total = tip;
 })
 
 twentyFivePercentBtn.addEventListener('click', () => {
-    let tip = ((value / 100) * 25).toFixed(2);
+    let tip = ((inputValue / 100) * 25).toFixed(2);
     tipPerPerson.innerHTML = tip;
+    total = tip;
 })
 
 fiftyPercentBtn.addEventListener('click', () => {
-    let tip = ((value / 100) * 50).toFixed(2);
+    let tip = ((inputValue / 100) * 50).toFixed(2);
     tipPerPerson.innerHTML = tip;
+    total = tip;
 })
 
 customBtn.addEventListener('input', (e) => {
